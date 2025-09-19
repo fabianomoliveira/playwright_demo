c637e3c06bf74bcae491959f104fc6a774444959
Fabiano Oliveira <fmoliveira@gmail.com>
2025-09-19 08:27:29 -0300

    force headless

--- browserSetup.ts
+++ browserSetup.ts
@@ -5,7 +5,7 @@ let page: Page;
 let browser: Browser;
 
 Before(async () => {
-  browser = await chromium.launch({ headless: false });
+  browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   page = await context.newPage();
 
